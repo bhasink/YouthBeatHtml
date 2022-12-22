bf724132d32e4f6040e38bb57c14685ff8e79f04
Ajeet <jeetay.yadav284@gmail.com>
2022-12-22 16:23:41 +0530

    inner page added

--- custome.js
+++ custome.js
@@ -265,6 +265,65 @@ $(document).ready(function () {
 })
 
 
+
+/* sliders testimonials new yt  */
+
+$(document).ready(function () {
+  var owl = $('.newtestslide');
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    nav:false,
+    margin: 50,
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: true,
+   
+    responsive: {
+      0: {
+        items: 1.5,
+		stagepadding:40,
+        nav: false,
+        dots: true,
+        loop: true
+      },
+      300: {
+        items: 1.5,
+		stagepadding:40,
+        nav: false,
+		autoplay: true,
+		 center: true,
+        dots: false,
+        loop: true
+      },
+
+      766: {
+        items: 2,
+        nav: false,
+        dots: false,
+        loop: true
+      },
+
+      1200: {
+        items: 3,
+        nav: false,
+        dots: false,
+        loop: false
+      }
+    }
+  });
+  $('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [3000])
+  })
+  $('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+  })
+
+
+})
+
+
 /* sliders for Partners  */
 
 $(document).ready(function () {
@@ -331,7 +390,7 @@ $(document).ready(function () {
     nav:false,
     margin: 20,
     dots: true,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     autoplayHoverPause: true,
    
@@ -362,7 +421,64 @@ $(document).ready(function () {
         items: 1,
         nav: true,
         dots: false,
-        loop: false
+        loop: true
+      }
+    }
+  });
+  $('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [3000])
+  })
+  $('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+  })
+
+
+})
+
+
+/* slider for mobile showcase  */
+
+$(document).ready(function () {
+  var owl = $('.mobilessslide');
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    nav:false,
+    margin: 20,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    autoplayHoverPause: true,
+	animateIn: 'fadeIn',
+	animateOut: 'fadeOut',
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: true,
+        loop: true
+      },
+      300: {
+        items: 1,
+        nav: false,
+		 center: false,
+		 autoplay: true,
+       dots: false,
+        loop: true
+      },
+
+      766: {
+        items: 1,
+        nav: false,
+        dots: false,
+        loop: true
+      },
+
+      1200: {
+        items: 1,
+        nav: false,
+        dots: false,
+        loop: true
       }
     }
   });
